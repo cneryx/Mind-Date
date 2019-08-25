abf46014e7226a7d37f8ebb2865f477f88916ca9
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-08-24 22:45:42 -0400

    aefdsghjgkh
    I'm am have cancer

--- expressServer.js
+++ expressServer.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.use(express.static('templates'));
+app.use(express.static('css'));
 app.set('view engine', 'pug');
-app.set('views', 'templates');
+app.set('views', './');
+
 
 //app.get('/', (req, res) => res.send('Hello World!'));
 
